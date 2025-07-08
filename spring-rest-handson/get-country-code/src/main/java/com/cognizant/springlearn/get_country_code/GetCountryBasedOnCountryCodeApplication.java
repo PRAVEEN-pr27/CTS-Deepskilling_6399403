47c488e7850809b6cbbe3da54117657be94a9811
PRAVEEN-pr27 <prpraveen04@gmail.com>
2025-07-08 14:56:43 +0530

    Upload Week 4 CTS Java FSE hands-on

---
+++ spring-rest-handson/get-country-code/src/main/java/com/cognizant/springlearn/get_country_code/GetCountryBasedOnCountryCodeApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.springlearn.get_country_code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetCountryBasedOnCountryCodeApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GetCountryBasedOnCountryCodeApplication.class, args);
+	}
+}
+
+
